6c011bd66019c7a030d6c9d39332e0d64f4b8cc1
Maciej Sadowski <maciej.sadowski@outlook.com>
2024-08-25 21:52:21 +0200

    Dashboard bugfix

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,25 +3,34 @@
 
 @section('inner-content')
 
-<section>
-    <div class="loginform">
+    <section>
+        <div class="container-fluid login-container">
+            <div class="row justify-content-center">
+                <div class="col-md-4 col-sm-8 col-xs-12">
+                    <div class="login-form">
+                        <h2>Logowanie</h2>
+                        <form action="{{ route('login') }}" method="POST">
+                            <div class="form-group">
+                                @csrf
 
-        <form action="{{ route('login') }}" method="POST">
-            @csrf
+                                <input name="username" type="text" placeholder="Login" onfocus="this.placeholder=''"
+                                    onblur="this.placeholder='Login'" class="form-control">
+                                <input name="password" type="password" placeholder="Hasło" onfocus="this.placeholder=''"
+                                    onblur="this.placeholder='Hasło'" required autocomplete="current-password"
+                                    class="form-control">
 
-            <input name="username" type="text" placeholder="Login" onfocus="this.placeholder=''"
-                onblur="this.placeholder='Login'" >
-            <input name="password" type="password" placeholder="Hasło" onfocus="this.placeholder=''"
-                onblur="this.placeholder='Hasło'" required autocomplete="current-password">
-
-            <label for="remember_me">
-                <input id="remember_me" type="checkbox"  name="remember">
-                <span>Zapamiętaj</span>
-            </label>
-
-            <input name="loginBtn" type="submit" value="Zaloguj się">
-        </form>
-    </div>
-</section>
+                                <div class="form-check">
+                                    <input id="remember_me" type="checkbox" name="remember" class="form-check-input">
+                                    <label for="remember_me" class="form-check-label">Zapamiętaj</label>
+                                </div>
+                                
+                            </div>
+                            <input name="loginBtn" type="submit" value="Zaloguj się" class="btn btn-primary">
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 
 @endsection
