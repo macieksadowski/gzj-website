6db1dbca5047c12ba4ec338dedbf2a210b11a59d
Maciej Sadowski <maciej.sadowski@outlook.com>
2024-09-03 23:58:57 +0200

    Fixed grid on mobile devices

--- resources/views/dashboard-sections/contracts-generator.blade.php
+++ resources/views/dashboard-sections/contracts-generator.blade.php
@@ -5,45 +5,49 @@
     <section>
         <div class="dashboard__generator">
 
-            <form method="get">
+            <form method="get" class="col-12">
                 @csrf
-                <div class="form-group row">
-                    <label for="contract-person-select">Na kogo ma być wygenerowana umowa?</label>
-                    <select onchange="this.form.submit()" id="contract-person-select" name="id" class="form-select">
-                        @foreach ($members as $member)
-                            <option @if ($member->id == $selected->id) selected @endif value={{ $member->id }}>
-                                {{ $member->first_name }} {{ $member->last_name }}</option>';
-                        @endforeach
-                    </select>
+                <div class="row">
+                    <div class="form-group col-12">
+                        <label for="contract-person-select">Na kogo ma być wygenerowana umowa?</label>
+                        <select onchange="this.form.submit()" id="contract-person-select" name="id" class="form-select">
+                            @foreach ($members as $member)
+                                <option @if ($member->id == $selected->id) selected @endif value={{ $member->id }}>
+                                    {{ $member->first_name }} {{ $member->last_name }}</option>';
+                            @endforeach
+                        </select>
+                    </div>
                 </div>
             </form>
-            <form method="post">
+            <form method="post" class="col-12">
                 @csrf
                 <input type="hidden" name="member_id" value="{{ $selected->id }}">
                 <fieldset class="row mb-3">
                     <legend class="col-form-label col-sm-2 pt-0">Typ umowy</p>
-                    <div class="col-sm-10">
-                        <div class="form-check">
-                            <input type="radio" id="zlecenie" name="contractType" value="zlecenie" checked class="form-check-input">
-                            <label for="zlecenie" class="form-check-label">Zlecenie</label>
+                        <div class="col-sm-10">
+                            <div class="form-check">
+                                <input type="radio" id="zlecenie" name="contractType" value="zlecenie" checked
+                                    class="form-check-input">
+                                <label for="zlecenie" class="form-check-label">Zlecenie</label>
+                            </div>
+                            <div class="form-check">
+                                <input type="radio" id="dzielo" name="contractType" value="dzielo"
+                                    class="form-check-input">
+                                <label class="item" for="dzielo" class="form-check-label">O dzieło</label>
+                            </div>
                         </div>
-                        <div class="form-check">                    
-                            <input type="radio" id="dzielo" name="contractType" value="dzielo" class="form-check-input">
-                            <label class="item" for="dzielo" class="form-check-label">O dzieło</label>
-                        </div>
-                    </div>
                 </fieldset>
                 <div class="row g-3" id="personal-data">
-                        <div class="col-6">
-                            <label for="first_name" class="form-label">Imię</label>
-                            <input disabled class="form-control" type="text" id="first_name" name="data[first_name]"
-                                value="{{ $selected->first_name }}">
-                        </div>
-                        <div class="col-6">
-                            <label for="last_name" class="form-label">Nazwisko</label>
-                            <input disabled class="form-control" type="text" id="last_name" name="data[last_name]"
-                                value="{{ $selected->last_name }}">
-                        </div>
+                    <div class="col-6">
+                        <label for="first_name" class="form-label">Imię</label>
+                        <input disabled class="form-control" type="text" id="first_name" name="data[first_name]"
+                            value="{{ $selected->first_name }}">
+                    </div>
+                    <div class="col-6">
+                        <label for="last_name" class="form-label">Nazwisko</label>
+                        <input disabled class="form-control" type="text" id="last_name" name="data[last_name]"
+                            value="{{ $selected->last_name }}">
+                    </div>
                     <div class="col-9">
                         <label for="street" class="form-label">Ulica</label>
                         <input disabled class="form-control" type="text" id="street" name="data[street]"
@@ -85,19 +89,29 @@
                             value="{{ $selected->tax_office }}">
                     </div>
                 </div>
-                <div class="dashboard__generator__footer">
-                    <input class="form-control" name="fileName" type="text" placeholder="Nazwa pliku" onfocus="this.placeholder=''"
-                        onblur="this.placeholder='Nazwa pliku'">
-                    <input name="edit" type="button"  id="button-edit" value="Zmień dane"  class="btn btn-primary">
-                    <input type="button" name="copy" id="button-copy" value="Kopiuj dane"  class="btn btn-primary">
-                    <input formaction="{{ route('generateContract') }}" name="generate" type="submit"
-                        value="Generuj dokument"  class="btn btn-primary">
+                <div class="dashboard__generator__footer row">
+                    <div class="col-12 col-lg-6">
+                        <input class="form-control" name="fileName" type="text" placeholder="Nazwa pliku"
+                            onfocus="this.placeholder=''" onblur="this.placeholder='Nazwa pliku'">
+                    </div>
+                    <div class="col-12 col-lg-auto">
+                        <input formaction="{{ route('generateContract') }}" name="generate" type="submit"
+                            value="Generuj dokument" class="btn btn-primary">
+                    </div>
+                    <div class="col-12 col-lg-auto">
+                        <input name="edit" type="button" id="button-edit" value="Zmień dane"
+                            class="btn btn-primary">
+                        <input type="button" name="copy" id="button-copy" value="Kopiuj dane"
+                            class="btn btn-primary">
+                        
+                    </div>
                 </div>
             </form>
         </div>
     </section>
     <!-- Modal -->
-    <div class="modal fade" tabindex="-1"  id="clipboardModal" aria-labelledby="clipboardModalLabel" aria-hidden="true">
+    <div class="modal fade" tabindex="-1" id="clipboardModal" aria-labelledby="clipboardModalLabel"
+        aria-hidden="true">
         <div class="modal-dialog modal-dialog-centered">
             <div class="modal-content">
                 <div class="modal-header">
@@ -112,6 +126,6 @@
                 </div>
             </div>
         </div>
-    </div>                    
+    </div>
 
 @endsection
