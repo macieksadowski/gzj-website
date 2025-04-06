e740e95925dc35fb0413f1348f0d2f9fdb8a17b2
Maciej Sadowski <maciej.sadowski@outlook.com>
2025-04-06 15:53:41 +0200

    API for Angular Contracts Generator

--- app/Http/Controllers/MemberController.php
+++ app/Http/Controllers/MemberController.php
@@ -22,4 +22,33 @@ class MemberController extends Controller
         });
         return response()->json($members);
     }
+
+    public function createMember(Request $request) {
+        $validatedData = $this->validateMemberData($request);
+        $member = Member::create($validatedData);
+        return response()->json($member, 201);
+    }
+
+    public function editMember(Request $request, $id) {
+        $member = Member::findOrFail($id);
+        $validatedData = $this->validateMemberData($request);
+
+        $member->update($validatedData);
+        return response()->json($member);
+    }
+
+    private function validateMemberData($data) {
+        return $data->validate([
+            'first_name' => 'required|string|max:255',
+            'last_name' => 'required|string|max:255',
+            'street' => 'nullable|string|max:255',
+            'house_no' => 'nullable|string|max:10',
+            'postal_code' => 'nullable|string|max:10',
+            'town' => 'nullable|string|max:255',
+            'pesel' => 'nullable|digits:11',
+            'birth_place' => 'nullable|string|max:255',
+            'account_no' => 'nullable|string|max:34',
+            'tax_office' => 'nullable|string|max:255',
+        ]);
+    }
 }
--- app/Models/Member.php
+++ app/Models/Member.php
@@ -9,6 +9,19 @@ class Member extends Model
 {
     use HasFactory;
 
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'street',
+        'house_no',
+        'postal_code',
+        'town',
+        'pesel',
+        'birth_place',
+        'account_no',
+        'tax_office'
+    ];
+
     public function contracts() {
         return $this->hasMany(Contract::class);
     }
